a225d89269b8a6dddf5766133bc1ecb4ba82147a
ibrahim katari <ibrahim.katary@gmail.com>
2020-07-27 18:03:20 +0800

    adding the assests and the fonts

---
+++ lib/util/style.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const kPurpleColor = Color(0xffC52FF0);
+const kBlueColor = Color(0xff33E1FF);
+const kYellowColor = Color(0xFFF6B039);
+const kRedColor = Color(0xFFFF7474);
+
+// Typographys
+const kGoogle_style = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  color: Colors.white,
+);
+const kBubblegum_sans1 = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w500,
+  color: Colors.white,
+);
+const kBubblegum_sans2 = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  color: Colors.white,
+);
+const kBubblegum_sans3 = TextStyle(
+  fontSize: 15,
+  fontWeight: FontWeight.w500,
+  color: Colors.white,
+);
