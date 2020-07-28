523bdd5363740ff89c3a0d6ef6db1d882cb304d1
ibrahim katari <ibrahim.katary@gmail.com>
2020-07-29 03:45:04 +0800

    cerate child screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:save_kids/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:save_kids/models/child_avatar.dart';
 import 'package:save_kids/screens/child_screen/crete_child_provide.dart';
 
-import 'package:save_kids/screens/parent_screens/parent_dashboard.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,39 +14,46 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      supportedLocales: [
-        Locale('en', 'US'),
-        Locale('de', 'DE'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AvatarData(),
+        ),
       ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        supportedLocales: [
+          Locale('en', 'US'),
+          Locale('de', 'DE'),
+        ],
 
-      localizationsDelegates: [
-        // A class which loads the translations from JSON files
-        AppLocalizations.delegate,
-        // Built-in localization of basic text for Material widgets
-        GlobalMaterialLocalizations.delegate,
-        // Built-in localization for text direction LTR/RTL
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      // Returns a locale which will be used by the app
-      localeResolutionCallback: (locale, supportedLocales) {
-        // Check if the current device locale is supported
-        for (var supportedLocale in supportedLocales) {
-          if (supportedLocale.languageCode == locale.languageCode &&
-              supportedLocale.countryCode == locale.countryCode) {
-            return supportedLocale;
+        localizationsDelegates: [
+          // A class which loads the translations from JSON files
+          AppLocalizations.delegate,
+          // Built-in localization of basic text for Material widgets
+          GlobalMaterialLocalizations.delegate,
+          // Built-in localization for text direction LTR/RTL
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        // Returns a locale which will be used by the app
+        localeResolutionCallback: (locale, supportedLocales) {
+          // Check if the current device locale is supported
+          for (var supportedLocale in supportedLocales) {
+            if (supportedLocale.languageCode == locale.languageCode &&
+                supportedLocale.countryCode == locale.countryCode) {
+              return supportedLocale;
+            }
           }
-        }
-        // If the locale of the device is not supported, use the first one
-        // from the list (English, in this case).
-        return supportedLocales.first;
-      },
-      home: ChildScreen(),
+          // If the locale of the device is not supported, use the first one
+          // from the list (English, in this case).
+          return supportedLocales.first;
+        },
+        home: ChildScreen(),
+      ),
     );
   }
 }
--- lib/screens/child_screen/crete_child_provide.dart
+++ lib/screens/child_screen/crete_child_provide.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import 'package:save_kids/app_localizations.dart';
+import 'package:save_kids/models/child_avatar.dart';
 import 'package:save_kids/util/style.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -11,7 +13,8 @@ class ChildScreen extends StatefulWidget {
 
 class _ChildScreenState extends State<ChildScreen> {
   var childname = " ";
-  var selectedIndex = "";
+  var selectedIndex = -1;
+
   @override
   Widget build(BuildContext context) {
     final text = AppLocalizations.of(context);
@@ -24,111 +27,235 @@ class _ChildScreenState extends State<ChildScreen> {
       ),
       backgroundColor: kBlueColor,
       extendBodyBehindAppBar: true,
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Stack(
-          children: <Widget>[
-            Opacity(
-              opacity: 0.15,
-              child: SvgPicture.asset(
-                "images/svgs/Asset1.svg",
-                fit: BoxFit.cover,
+      body: Consumer<AvatarData>(
+        builder: (context, avatarData, child) => Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: <Widget>[
+              Opacity(
+                opacity: 0.10,
+                child: SvgPicture.asset(
+                  "images/svgs/Asset1.svg",
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Text(
-                  text.translate('add_child_screen_title_1'),
-                  style: GoogleFonts.bubblegumSans(textStyle: kBubblegum_sans1),
-                ),
-                CircleAvatar(
-                  radius: 60,
-                  backgroundColor: Colors.black,
-                ),
-                Form(
-                  child: Container(
-                    margin: EdgeInsets.symmetric(
-                        horizontal: MediaQuery.of(context).size.width * 0.12),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: "Child Name",
-                        labelStyle: GoogleFonts.bubblegumSans(
-                                textStyle: kBubblegum_sans2)
-                            .copyWith(
-                          fontSize: 20,
-                          color: Colors.black,
-                        ),
-                        focusColor: Colors.black,
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            width: 3,
-                            color: Colors.black,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Text(
+                    text.translate('Create_a_profile_for_your_kids'),
+                    style:
+                        GoogleFonts.bubblegumSans(textStyle: kBubblegum_sans1),
+                  ),
+                  FlatButton(
+                    onPressed: () => showDialog(
+                      context: context,
+                      builder: (BuildContext _) => StatefulBuilder(
+                        builder: (context, setStaste) => Dialog(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(48),
+                          ),
+                          child: Container(
+                            height: MediaQuery.of(context).size.height - 100,
+                            child: Column(
+                              children: <Widget>[
+                                SizedBox(
+                                  height: 60,
+                                ),
+                                CircleAvatar(
+                                  backgroundColor: Colors.yellow[50],
+                                  radius: 60,
+                                  backgroundImage: NetworkImage(
+                                    avatarData.avatars[avatarData.currentAvatar]
+                                        .childAvatar,
+                                  ),
+                                ),
+                                Flexible(
+                                  child: GridView.count(
+                                    primary: false,
+                                    padding: const EdgeInsets.all(20),
+                                    crossAxisSpacing: 10,
+                                    mainAxisSpacing: 10,
+                                    crossAxisCount: 3,
+                                    children: List<Widget>.generate(
+                                      avatarData.avatars.length,
+                                      (index) => GestureDetector(
+                                        onTap: () {
+                                          setStaste(() {
+                                            avatarData.setCurrentAvatr(
+                                                avatarData
+                                                    .avatars[index].index);
+                                          });
+                                        },
+                                        child: Stack(
+                                          children: <Widget>[
+                                            CircleAvatar(
+                                              radius: 60,
+                                              backgroundColor:
+                                                  avatarData.currentAvatar ==
+                                                          index
+                                                      ? kPurpleColor
+                                                      : kYellowColor,
+                                            ),
+                                            Center(
+                                              child: CircleAvatar(
+                                                backgroundColor: Colors.white,
+                                                radius: 40,
+                                                backgroundImage: NetworkImage(
+                                                  avatarData.avatars[index]
+                                                      .childAvatar,
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () => Navigator.pop(context),
+                                  child: AgeChip(
+                                    color: kBluesColor,
+                                    text: "Done",
+                                    highet: 60.0,
+                                    width: 120.0,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 60,
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                      onChanged: (val) {
-                        childname = val;
-                      },
                     ),
-                  ),
-                ),
-                Column(
-                  children: <Widget>[
-                    Text(
-                      text.translate('add_child_screen_title_2'),
-                      style: GoogleFonts.bubblegumSans(
-                          textStyle: kBubblegum_sans1),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        AgeChip(
-                          index: 0,
-                          highet: 57.00,
-                          width: 110.00,
-                          text: text.translate('add_child_screen_title_3'),
-                          // text: " ",
-                        ),
-                        AgeChip(
-                          index: 1,
-                          highet: 57.00,
-                          width: 110.00,
-                          text: "5-7",
-                        ),
-                        AgeChip(
-                          index: 1,
-                          highet: 57.00,
-                          width: 100.00,
-                          text: "8-12",
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                Container(
-                  height: 58.00,
-                  width: 226.00,
-                  decoration: BoxDecoration(
-                    color: Color(0xfff6b039),
-                    borderRadius: BorderRadius.circular(50.00),
-                  ),
-                  child: Center(
-                    child: Text(
-                      text.translate('yellow_button'),
-                      style: GoogleFonts.bubblegumSans(
-                        textStyle: kBubblegum_sans2.copyWith(),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 60,
+                      backgroundImage: NetworkImage(
+                        avatarData
+                            .avatars[avatarData.currentAvatar].childAvatar,
                       ),
                     ),
                   ),
-                )
-              ],
-            ),
-          ],
+                  Form(
+                    child: Container(
+                      margin: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width * 0.12),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Child Name",
+                          labelStyle: GoogleFonts.bubblegumSans(
+                                  textStyle: kBubblegum_sans2)
+                              .copyWith(
+                            fontSize: 20,
+                            color: Colors.black,
+                          ),
+                          focusColor: Colors.black,
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              width: 3,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        onChanged: (val) {
+                          childname = val;
+                        },
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        text.translate('Age'),
+                        style: GoogleFonts.bubblegumSans(
+                            textStyle: kBubblegum_sans1),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                selectedIndex = 0;
+                                print(selectedIndex);
+                              });
+                            },
+                            child: AgeChip(
+                              color: selectedIndex == 0
+                                  ? kPurpleColor
+                                  : kBluesColor,
+                              highet: 57.00,
+                              width: 110.00,
+                              text: text.translate('4_or_less'),
+                              // text: " ",
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                selectedIndex = 1;
+                                print(selectedIndex);
+                              });
+                            },
+                            child: AgeChip(
+                              color: selectedIndex == 1
+                                  ? kPurpleColor
+                                  : kBluesColor,
+                              highet: 57.00,
+                              width: 110.00,
+                              text: "5-7",
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                selectedIndex = 2;
+                                print(selectedIndex);
+                              });
+                            },
+                            child: AgeChip(
+                              color: selectedIndex == 2
+                                  ? kPurpleColor
+                                  : kBluesColor,
+                              highet: 57.00,
+                              width: 100.00,
+                              text: "8-12",
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  Container(
+                    height: 58.00,
+                    width: 226.00,
+                    decoration: BoxDecoration(
+                      color: Color(0xfff6b039),
+                      borderRadius: BorderRadius.circular(50.00),
+                    ),
+                    child: Center(
+                      child: Text(
+                        text.translate('Finish'),
+                        style: GoogleFonts.bubblegumSans(
+                          textStyle: kBubblegum_sans1.copyWith(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -137,7 +264,7 @@ class _ChildScreenState extends State<ChildScreen> {
 
 class AgeChip extends StatelessWidget {
   const AgeChip({
-    this.index,
+    this.color,
     this.width,
     this.highet,
     this.text,
@@ -146,7 +273,7 @@ class AgeChip extends StatelessWidget {
   final highet;
   final width;
   final text;
-  final index;
+  final color;
 
   @override
   Widget build(BuildContext context) {
@@ -154,14 +281,16 @@ class AgeChip extends StatelessWidget {
       height: highet,
       width: width,
       decoration: BoxDecoration(
-        color: Color(0xff035AA6),
+        color: color,
         borderRadius: BorderRadius.circular(20.00),
       ),
       child: Center(
         child: Text(
           text,
           style: GoogleFonts.bubblegumSans(
-            textStyle: kBubblegum_sans2.copyWith(color: Colors.white),
+            textStyle: kBubblegum_sans2.copyWith(
+              color: Colors.white,
+            ),
           ),
         ),
       ),
