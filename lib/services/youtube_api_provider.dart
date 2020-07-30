5d5823ceb5ca2fbdbe433215b73fb17dcf6a4b92
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2020-07-30 18:10:12 +0800

    backend/bloc init

---
+++ lib/services/youtube_api_provider.dart
@@ -0,0 +1,8 @@
+import 'package:googleapis/youtube/v3.dart';
+import 'package:googleapis_auth/auth.dart';
+import 'package:googleapis_auth/auth_io.dart';
+
+class YoutubeApiProvider {
+  final baseURL =
+      "https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyAR3UzqbdDjzTh7QtfDnY5V3IThlSZxQaE";
+}
