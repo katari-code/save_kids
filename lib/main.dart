bc243a52eb36064e80fb960765c24dc5e577e569
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2020-08-01 02:51:56 +0800

    channel search bloc impmeneted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:save_kids/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:save_kids/bloc/channel_bloc.dart';
 import 'package:save_kids/models/channel.dart';
 import 'package:save_kids/models/child.dart';
 import 'package:save_kids/models/child_avatar.dart';
@@ -31,43 +33,48 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (_) => KidsData(),
         ),
-        ChangeNotifierProvider(
-          create: (_) => ChannelData(),
-        ),
+        // ChangeNotifierProvider(
+        //   create: (_) => ChannelData(),
+        // ),
       ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        supportedLocales: [
-          Locale('en', 'US'),
-          Locale('de', 'DE'),
+      child: BlocProvider(
+        blocs: [
+          Bloc((i) => ChannelBloc()),
         ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          supportedLocales: [
+            Locale('en', 'US'),
+            Locale('de', 'DE'),
+          ],
 
-        localizationsDelegates: [
-          // A class which loads the translations from JSON files
-          AppLocalizations.delegate,
-          // Built-in localization of basic text for Material widgets
-          GlobalMaterialLocalizations.delegate,
-          // Built-in localization for text direction LTR/RTL
-          GlobalWidgetsLocalizations.delegate,
-        ],
-        // Returns a locale which will be used by the app
-        localeResolutionCallback: (locale, supportedLocales) {
-          // Check if the current device locale is supported
-          for (var supportedLocale in supportedLocales) {
-            if (supportedLocale.languageCode == locale.languageCode &&
-                supportedLocale.countryCode == locale.countryCode) {
-              return supportedLocale;
+          localizationsDelegates: [
+            // A class which loads the translations from JSON files
+            AppLocalizations.delegate,
+            // Built-in localization of basic text for Material widgets
+            GlobalMaterialLocalizations.delegate,
+            // Built-in localization for text direction LTR/RTL
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          // Returns a locale which will be used by the app
+          localeResolutionCallback: (locale, supportedLocales) {
+            // Check if the current device locale is supported
+            for (var supportedLocale in supportedLocales) {
+              if (supportedLocale.languageCode == locale.languageCode &&
+                  supportedLocale.countryCode == locale.countryCode) {
+                return supportedLocale;
+              }
             }
-          }
-          // If the locale of the device is not supported, use the first one
-          // from the list (English, in this case).
-          return supportedLocales.first;
-        },
-        home: AccountsDashborasScreen(),
+            // If the locale of the device is not supported, use the first one
+            // from the list (English, in this case).
+            return supportedLocales.first;
+          },
+          home: ChannelScreen(),
+        ),
       ),
     );
   }
--- lib/models/channel.dart
+++ lib/models/channel.dart
@@ -44,43 +44,44 @@ class Channel implements Mapper {
   fromSearchMap(Map<String, dynamic> map) {
     return Channel.fromSearchMap(map);
   }
-import 'package:flutter/cupertino.dart';
-
-class Channel {
-  String channelId;
-  String channelName;
-  String channelAvatar;
-  String channelDesc;
-  bool isChose;
-
-  Channel({
-    this.channelId,
-    this.channelName,
-    this.channelDesc,
-    this.channelAvatar,
-    this.isChose,
-  });
 }
+// import 'package:flutter/cupertino.dart';
 
-class ChannelData with ChangeNotifier {
-  List<Channel> channels = [
-    Channel(
-      channelId: "001",
-      channelName: "Kids Channel",
-      channelAvatar:
-          "https://yt3.ggpht.com/a/AATXAJyttsvxVYq7Lu5KGRCK9J1QHA6fWDDhwXnIEPe7Nw=s176-c-k-c0x00ffffff-no-rj",
-      channelDesc:
-          "Kids Channel is an online educational platform, specializing in graphic videos, nursery rhymes and songs for children. ",
-      isChose: true,
-    ),
-    Channel(
-      channelId: "002",
-      channelName: "Loco Nuts",
-      channelAvatar:
-          "https://yt3.ggpht.com/a/AATXAJzPJy22RVQQJNASRQyzBbcSIMbmwuC7k7pvY7wg=s176-c-k-c0x00ffffff-no-rj",
-      channelDesc:
-          "he series revolves around the fun adventures of 6 animal friends",
-      isChose: false,
-    ),
-  ];
-}
+// class Channel {
+//   String channelId;
+//   String channelName;
+//   String channelAvatar;
+//   String channelDesc;
+//   bool isChose;
+
+//   Channel({
+//     this.channelId,
+//     this.channelName,
+//     this.channelDesc,
+//     this.channelAvatar,
+//     this.isChose,
+//   });
+// }
+
+// class ChannelData with ChangeNotifier {
+//   List<Channel> channels = [
+//     Channel(
+//       channelId: "001",
+//       channelName: "Kids Channel",
+//       channelAvatar:
+//           "https://yt3.ggpht.com/a/AATXAJyttsvxVYq7Lu5KGRCK9J1QHA6fWDDhwXnIEPe7Nw=s176-c-k-c0x00ffffff-no-rj",
+//       channelDesc:
+//           "Kids Channel is an online educational platform, specializing in graphic videos, nursery rhymes and songs for children. ",
+//       isChose: true,
+//     ),
+//     Channel(
+//       channelId: "002",
+//       channelName: "Loco Nuts",
+//       channelAvatar:
+//           "https://yt3.ggpht.com/a/AATXAJzPJy22RVQQJNASRQyzBbcSIMbmwuC7k7pvY7wg=s176-c-k-c0x00ffffff-no-rj",
+//       channelDesc:
+//           "he series revolves around the fun adventures of 6 animal friends",
+//       isChose: false,
+//     ),
+//   ];
+// }
