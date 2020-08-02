3483eee4c4625812833ab161daad38476c3485aa
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2020-08-02 19:35:50 +0800

    logger for debuggin added

--- lib/services/repository.dart
+++ lib/services/repository.dart
@@ -1,18 +1,31 @@
+import 'package:logger/logger.dart';
 import 'package:save_kids/models/channel.dart';
 import 'package:save_kids/models/video.dart';
 import 'package:save_kids/services/youtube_api_provider.dart';
 
 class Repository<T> {
   YoutubeApiProvider _youtubeApi;
+  Logger logger = Logger();
+
   Future<List<Video>> getVideosBySearch(String search) async {
     _youtubeApi = YoutubeApiProvider<Video>();
-    return _youtubeApi.fetchBySearch(
-        mapper: Video(), search: search, type: 'video');
+    try {
+      return _youtubeApi.fetchBySearch(
+          mapper: Video(), search: search, type: 'video');
+    } catch (e) {
+      logger.e(e);
+      return null;
+    }
   }
 
   Future<List<Channel>> getChannelsBySearch(String search) async {
     _youtubeApi = YoutubeApiProvider<Channel>();
-    return _youtubeApi.fetchBySearch(
-        mapper: Channel(), search: search, type: 'channel');
+    try {
+      return _youtubeApi.fetchBySearch(
+          mapper: Channel(), search: search, type: 'channel');
+    } catch (e) {
+      logger.e(e);
+      return null;
+    }
   }
 }
