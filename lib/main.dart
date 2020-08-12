456e043768513ea635b250927f4ef0ac90fb3c60
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2020-08-12 17:00:57 +0800

    watch schedule refactored

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:save_kids/bloc/auth_bloc.dart';
 import 'package:save_kids/bloc/channel_bloc.dart';
 import 'package:save_kids/bloc/create_child_profile_bloc.dart';
 import 'package:save_kids/bloc/test/video_list_bloc_test.dart';
+import 'package:save_kids/bloc/watch_schedule_bloc.dart';
 import 'package:save_kids/models/child.dart';
 import 'package:save_kids/models/child_avatar.dart';
 
@@ -62,6 +63,7 @@ class MyApp extends StatelessWidget {
           Bloc((i) => VideoListBloc()),
           Bloc((i) => AddVideoBloc()),
           Bloc((i) => AddScheduleBloc()),
+          Bloc((i) => WatchScheduleBloc()),
         ],
         child: MaterialApp(
           title: 'Save Video Kids',
@@ -94,7 +96,7 @@ class MyApp extends StatelessWidget {
             // from the list (English, in this case).
             return supportedLocales.first;
           },
-          initialRoute: kVideoDisplayRoute,
+          initialRoute: kWatchSchdeuleRoute,
           onGenerateRoute: (RouteSettings settings) {
             return createRoute(settings);
           },
